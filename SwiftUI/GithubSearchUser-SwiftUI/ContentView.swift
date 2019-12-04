1f27acf4fdac5f759a625bc0da498415c69eb182
Kyujin Kim <mildwhale@gmail.com>
2019-12-04 15:20:07 +0900

    SwiftUI Initial project commit.

---
+++ SwiftUI/GithubSearchUser-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubSearchUser-SwiftUI
+//
+//  Created by Kyujin Kim on 2019/12/04.
+//  Copyright © 2019 Wayne Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
