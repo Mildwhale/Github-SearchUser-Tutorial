3c13c7199d8ad49fd86e4482aafad5030ff3c6a6
Wayne Kim <mildwhale@gmail.com>
2020-05-10 09:29:35 +0900

    :sparkles: 아키텍처 리스트 화면을 구현.

---
+++ GithubSearchUser-Tutorial/Helper/Bundle+info.swift
@@ -0,0 +1,13 @@
+//
+//  Bundle+info.swift
+//  GithubSearchUser-Tutorial
+//
+//  Created by Wayne Kim on 2020/05/08.
+//  Copyright © 2020 Wayne Kim. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    static let clientId: String = "" // TODO:
+}
